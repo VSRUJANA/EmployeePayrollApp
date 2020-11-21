933065dbf1e8491bed1660a3f0c48e8962e316cd
VSRUJANA <srujana.valavala@gmail.com>
2020-11-21 22:32:28 +0530

    [srujana] Add. Set EventListener on SalaryRange to display appropriate value

--- javascript/EmployeePayrollForm.js
+++ javascript/EmployeePayrollForm.js
@@ -1,20 +1,28 @@
-
-
-// Event listener for salary
+// Event listener for Salary Range
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
-salary.addEventListener('input', function () {
+salary.addEventListener('input', function() 
+{
     output.textContent = salary.value;
 });
 
-//Event listener for name
+// Event listener for Name
 const name = document.querySelector('#name');
 const error = document.querySelector('.text-error');
-name.addEventListener('input', function () {
+name.addEventListener('input', function () 
+{
     let nameRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
     if (nameRegex.test(name.value))
         error.textContent = "";
     else
         error.textContent = "Invalid Name";
 });
+
+//
+function ResetValue()
+{
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = 400000;
+}
